aa264a236b8b9732346cbb4870ff29be4332e653
Michael Novajosky <mikenovajosky@yahoo.com>
2022-03-13 10:49:03 -0700

    remove unused files

--- src/pages/MessagesPage/MessagesPage.jsx
+++ src/pages/MessagesPage/MessagesPage.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import './MessagesPage.css'
-import Sidebar from '../../components/Sidebar/Sidebar';
 import MessageBoard from '../../components/MessageBoard/MessageBoard'
 
 export default function Messages() {
 
     return (
       <main>
-        {/* <div className="d-flex" style={{ height: '100vh' }}>
-          <Sidebar/>
-        </div> */}
         <div>
           <MessageBoard/>
         </div>
