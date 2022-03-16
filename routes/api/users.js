7a810cc163ce8d732fbbde526dae5da269ac781c
Michael Novajosky <mikenovajosky@yahoo.com>
2022-03-16 09:46:21 -0700

    refactor

--- routes/api/users.js
+++ routes/api/users.js
@@ -21,37 +21,5 @@ router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken);
 // GET user data
 router.get('/', usersCtrl.getAll);
 
-// router.post('/insert', function(req, res, next) {
-//     let item = {
-//         name: req.body.name,
-//         citystate: req.body.citystate,
-//         handicap: req.body.handicap,
-//         drinker: req.body.drinker,
-//         gender: req.body.gender,
-//         pfgender: req.body.pfgender,
-//         playage: req.body.playage,
-//         drinker: req.body.drinker,
-//         meticulous: req.body.meticulous,
-//         etiquette: req.body.etiquette,
-//         skillset: req.body.skillset,
-//     };
-
-//     mongo.connect(url, function() {
-//         assert.equal(null, err);
-//         db.collection('user-data').insertOne(item, function(err, result) {
-//             assert.equal(null, err);
-//             console.log('item inserted');
-//             db.close();
-//         })
-//     });
-
-//     res.redirect('/');
-// });
-router.post('/update', function(req, res, next) {
-
-});
-router.post('/delete', function(req, res, next) {
-
-});
 
 module.exports = router;
